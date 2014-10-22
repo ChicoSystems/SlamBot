6159cb5b1ce4f990c165d8bbeb6f43d3d026e88d
Isaac Assegai <confusedvirtuoso@gmail.com>
2014-10-22 14:21:07 -0700

    finishging up building multithread test

--- multithreading-test/multithreading-test.ino
+++ multithreading-test/multithreading-test.ino
@@ -39,10 +39,26 @@ void DistanceSensorCallback(){
 
 void setup(){
   Serial.begin(9600); 
+  //setup pins to distanceSensor
   distanceSensor.fPinWrite = A2;
-  distanceSensor.rPin = 
-  distanceSensor.bPin =
-  distanceSensor.lPin =
+  distanceSensor.fPinRead = A3;
+  distanceSensor.rPinWrite = 10; 
+  distanceSensor.rPinRead = 10;
+  distanceSensor.bPinWrite = 11;
+  distanceSensor.bPinRead = 11;
+  distanceSensor.lPinWrite = A0;
+  distanceSensor.lPinRead = A1;
+  
+  //set the interval that the class will run at.
+  distanceSensor.setInterval(250);
+  
+  //add the DistanceSensor to the Thread Controller using it's pointer
+  controller.add(&distanceSensor);
+  
+  //Initialize the timer used for multithreading.
+  Timer1.initialize(5000);
+  Timer1.attachInterrupt(timerCallback);
+  Timer1.start();
 }
 
 
