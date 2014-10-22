6f365426029420b846a5bd329ea60c86f0dc7977
Isaac Assegai <confusedvirtuoso@gmail.com>
2014-10-22 14:12:22 -0700

    added notes

--- multithreading-test/multithreading-test.ino
+++ multithreading-test/multithreading-test.ino
@@ -5,12 +5,20 @@
 
 class DistanceSensorThread: public Thread{
   public:
-    float f, r, b, l;
+    //Stores the value of the pings
+    float f, r, b, l; 
+    
+    //Tells us which pins to hook up to the four sensors.
+    int fPinWrite, fPinRead, rPinWrite, rPinRead,
+        bPinWrite, bPinRead, lPinWrite, lPinRead;
+        
+    //Constants that can be used to denote which sensor
     const int FRONT = 0;
     const int RIGHT = 1;
     const int BACK = 2;
     const int LEFT = 3;
     
+    //Used by the multi-threading library
     void run(){
       f = ping(FRONT);
       r = ping(RIGHT);
@@ -20,11 +28,21 @@ class DistanceSensorThread: public Thread{
     }
 };
 
+//Initiate our distance sensor and the controller
 DistanceSensorThread distanceSensor = DistanceSensorThread();
 ThreadController controller = ThreadController();
 
+//The function that the timer will call for the DistanceSensor.
 void DistanceSensorCallback(){
   distanceSensor.run(); 
+}
+
+void setup(){
+  Serial.begin(9600); 
+  distanceSensor.fPinWrite = A2;
+  distanceSensor.rPin = 
+  distanceSensor.bPin =
+  distanceSensor.lPin =
 }
 
 
