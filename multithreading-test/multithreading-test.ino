4e707fc256e0a9bbc1400a91ba93ccfcaee14c67
Isaac Assegai <confusedvirtuoso@gmail.com>
2014-10-22 14:01:53 -0700

    updating DistanceSensorThread Class

--- multithreading-test/multithreading-test.ino
+++ multithreading-test/multithreading-test.ino
@@ -3,5 +3,20 @@
 #include "ThreadController.h"
 #include <TimerOne.h>
 
-
+class DistanceSensorThread: public Thread{
+  public:
+    float f, r, b, l;
+    const int FRONT = 0;
+    const int RIGHT = 1;
+    const int BACK = 2;
+    const int LEFT = 3;
+    
+    void run(){
+      f = ping(FRONT);
+      r = ping(RIGHT);
+      b = ping(BACK);
+      l = ping(LEFT); 
+      runned();
+    }
+}
 
