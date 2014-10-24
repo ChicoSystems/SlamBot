dbcaa6fc02106a830b904f89fa3143ff4608b111
Isaac Assegai <confusedvirtuoso@gmail.com>
2014-10-23 18:02:39 -0700

    re-arranged project, renamed to SlamBot,
    how do i change the name of the repository?

---
+++ RedBot.h
@@ -0,0 +1,78 @@
+#ifndef RedBot_h
+#define RedBot_h
+
+#include <Arduino.h>
+
+#define    L_CTRL_1   2
+#define    L_CTRL_2   4
+#define    PWM_L      5
+
+#define    R_CTRL_1   7
+#define    R_CTRL_2   8
+#define    PWM_R      6
+
+class RedBotMotor
+{
+  public:
+    RedBotMotor();
+    void drive(int speed);
+    void pivot(int speed);
+    void rightDrive(int speed);
+    void leftDrive(int speed);
+    void stop();
+    void brake();
+    void rightStop();
+    void leftStop();
+    void leftBrake();
+    void rightBrake();
+  private:
+    void leftFwd(byte speed);
+    void leftRev(byte speed);
+    void rightFwd(byte speed);
+    void rightRev(byte speed);
+};
+
+class RedBotSensor
+{
+  public:
+    RedBotSensor(int pin);
+    int read();
+    boolean check();
+    int setBGLevel();
+    int setDetectLevel();
+  private:
+    int _pin;
+    int _BGLevel;
+    int _BGSD;
+    int _detectLevel;
+    int _detectSD;
+};
+// I2C address of the MMA8452Q accelerometer
+#define XL_ADDR     0x1D 
+// I2C read bit set
+#define I2C_READ    0x01 
+// Some values we'll load into TWCR a lot
+// (1<<TWINT) | (1<<TWSTA) | (1<<TWEN)
+#define START_COND  0xA4 
+// (1<<TWINT) | (1<<TWSTO) | (1<<TWEN)
+#define STOP_COND   0x94 
+// (1<<TWINT) | (1<<TWEN)
+#define CLEAR_TWINT 0x84 
+// (1<<TWINT) | (1<<TWEA) | (1<<TWEN)
+#define NEXT_BYTE   0xC4 
+class RedBotAccel
+{
+  public:
+    RedBotAccel();
+    void read();
+    void enableBump();
+    boolean checkBump();
+    void setBumpThresh(int xThresh);
+    int x;
+    int y;
+    int z;
+  private:
+    void xlWriteBytes(byte addr, byte *buffer, byte len);
+    void xlReadBytes(byte addr, byte *buffer, byte len);
+};
+#endif// RedBot_h
---
+++ SlamBot.ino
@@ -0,0 +1,55 @@
+//** Experiementing with multithreading library ArduinoThread */
+#include "Thread.h"
+#include "ThreadController.h"
+#include "DistanceSensorThread.h"
+#include "TimerOne.h"
+#include "RedBot.h"
+
+
+//Initiate our distance sensor and the controller
+DistanceSensorThread distanceSensor = DistanceSensorThread();
+ThreadController controller = ThreadController();
+
+//The function that the timer will call for the DistanceSensor.
+void timerCallback(){
+  Serial.println(" timerCallback() ");
+  controller.run(); 
+}
+
+void setup(){
+  Serial.begin(9600); 
+  //setup pins to distanceSensor
+  distanceSensor.fPinWrite = A2;
+  distanceSensor.fPinRead = A3;
+  distanceSensor.rPinWrite = 10; 
+  distanceSensor.rPinRead = 10;
+  distanceSensor.bPinWrite = 11;
+  distanceSensor.bPinRead = 11;
+  distanceSensor.lPinWrite = A0;
+  distanceSensor.lPinRead = A1;
+  
+  //set the interval that the class will run at.
+  distanceSensor.setInterval(100);//in milliseconds
+  
+  //add the DistanceSensor to the Thread Controller using it's pointer
+  controller.add(&distanceSensor);
+  
+  //Initialize the timer used for multithreading.
+  Timer1.initialize(500000); //in microseconds
+  Timer1.attachInterrupt(timerCallback);
+}
+
+void loop(){
+  // Here is where the Threads are processed
+  delay(500);
+  Serial.print(" F: ");
+  Serial.print(distanceSensor.f);
+  Serial.print(" R: ");
+  Serial.print(distanceSensor.r);
+  Serial.print(" B: ");
+  Serial.print(distanceSensor.b);
+  Serial.print(" L: ");
+  Serial.println(distanceSensor.l);
+  //More Thread Processing done, why break it in half?
+  delay(500);
+}
