9a55088f2938c8d8bf5d8e50eafb5b4df5a7f1a1
Isaac Assegai <confusedvirtuoso@gmail.com>
2014-10-24 13:17:04 -0700

    Revert "Revert "changed debug""
    This reverts commit 6c09ffc334d728cca9c1035a60ce96200f0a15bb.

--- DistanceSensorThread.h
+++ DistanceSensorThread.h
@@ -15,7 +15,7 @@ class DistanceSensorThread: public Thread{
     
     //Used by the multi-threading library
     void run(){
-      //Serial.println("run()");
+     // Serial.print(" run() ");
       f = ping(0);
       r = ping(1);
       b = ping(2);
