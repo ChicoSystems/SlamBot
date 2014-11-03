9fe9ec2e6ebb3cf7da3b537ed3cf08aa82976cf6
Isaac Assegai <confusedvirtuoso@gmail.com>
2014-11-03 11:49:29 -0800

    adding debug statement, not working yet.

--- SlamBot.ino
+++ SlamBot.ino
@@ -15,6 +15,7 @@
 #define BEEPER A7
 #define BUMPER A6
 #define arduinoLED 13   // Arduino LED on board
+#define DEBUG
 
 // Instantiate the motors.
 RedBotMotor motors;
@@ -40,16 +41,16 @@ void timerCallback(){
 
 void setup(){
   Serial.begin(9600); 
-  Serial.println("SLAMBOT");
+  debug("SLAMBOT", true);
   
   tone(BEEPER, 2600, 750);
   
   // Send out a hello via the XBee, if one is present.
  // swsp.begin(9600);
   //swsp.print("Hello, world");
-  Serial.println("initializing compass");
+  debug(" initializing compass ", 1);
   compass.init();
-  Serial.println("compajss initialized");
+  debug(" compajss initialized ");
   //setup pins to distanceSensor
   distanceSensor.fPinWrite = A2;
   distanceSensor.fPinRead = A3;
@@ -64,15 +65,15 @@ void setup(){
   distanceSensor.setInterval(100);//in milliseconds
   compass.setInterval(100);
   
-  Serial.println("adding distance sensor");
+  debug(" adding distance sensor ");
   //add the DistanceSensor to the Thread Controller using it's pointer
   controller.add(&distanceSensor);
   controller.add(&compass);
-  Serial.println("initializing timer");
+  debug(" initializing timer" );
   //Initialize the timer used for multithreading.
   Timer1.initialize(100000); //in microseconds
   Timer1.attachInterrupt(timerCallback);
-  Serial.println("timer initialized");
+  debug(" timer initialized ", 1);
   
   //encoders don't work unless we power motors first.
   motors.brake();
@@ -82,7 +83,7 @@ void setup(){
   
   encoder.clearEnc(BOTH);
   goalHeading = compass.getHeading();
-  Serial.println(goalHeading);
+  debug(goalHeading);
   
   //add commands to allow remote control over serial
   sCmd.addCommand("ON",    LED_on);          // Turns LED on
@@ -125,15 +126,15 @@ void processTurnCommand() {
   int aNumber;
   char *arg;
 
-  Serial.println("We're in processTurnCommand");
+  debug(" We're in processTurnCommand ", 1);
   arg = sCmd.next();
   if (arg != NULL) {
     aNumber = atoi(arg);    // Converts a char string to an integer
-    Serial.print("First argument was: ");
-    Serial.println(aNumber);
+    debug(" First argument was: ");
+    debug(aNumber, 1);
     turnTo(aNumber, 0);
   }else {
-    Serial.println("No arguments");
+    debug("No arguments", 1);
   }
 }
 
@@ -144,53 +145,53 @@ void processMoveCommand() {
   int aNumber;
   char *arg;
 
-  Serial.println("We're in processMoveCommand");
+  debug("We're in processMoveCommand", 1);
   arg = sCmd.next();
   if (arg != NULL) {
     aNumber = atoi(arg);    // Converts a char string to an integer
-    Serial.print("First argument was: ");
-    Serial.println(aNumber);
+    debug("First argument was: ");
+    debug(aNumber, 1);
     dir = aNumber;
     arg = sCmd.next();
     if (arg != NULL) {
       aNumber = atoi(arg);    // Converts a char string to an integer
-      Serial.print("2nd argument was: ");
-      Serial.println(aNumber);
+      debug("2nd argument was: ");
+      debug(aNumber, 1);
       ticks = aNumber;
       arg = sCmd.next();
       if (arg != NULL) {
         aNumber = atoi(arg);    // Converts a char string to an integer
-        Serial.print("3rd argument was: ");
-        Serial.println(aNumber);
+        debug("3rd argument was: ");
+        debug(aNumber, 1);
         blocked_dist_cm = aNumber;
         arg = sCmd.next();
         if (arg != NULL) {
           aNumber = atoi(arg);    // Converts a char string to an integer
-          Serial.print("4th argument was: ");
-          Serial.println(aNumber);
+          debug("4th argument was: ");
+          debug(aNumber, 1);
           sp = aNumber;
         }else{
-          Serial.println("No 4th Argument");
+          debug("No 4th Argument", 1);
         }
       }else{
-        Serial.println("No 3nd Argument");
+        debug("No 3nd Argument", 1);
       }
     }else{
-      Serial.println("No 2nd Argument");
+      debug("No 2nd Argument", 1);
     }
   }else {
-    Serial.println("No First Argument");
+    debug("No First Argument", 1);
   }
   move(dir, ticks, blocked_dist_cm, sp);
 }
 
 void LED_on() {
-  Serial.println("LED on");
+  debug("LED on", 1);
   digitalWrite(arduinoLED, HIGH);
 }
 
 void LED_off() {
-  Serial.println("LED off");
+  debug("LED off", 1);
   digitalWrite(arduinoLED, LOW);
 }
 
@@ -200,14 +201,14 @@ void LED_off() {
   * the bot will move at the speed directed 0-255
   */
 void move(int dir, int dist_ticks, int blocked_dist_cm, int speed){
-  Serial.print("move()- dir:");
-  Serial.print(dir);
-  Serial.print(" dist_ticks:");
-  Serial.print(dist_ticks);
-  Serial.print(" blocked_dist_cm:");
-  Serial.print(blocked_dist_cm);
-  Serial.print(" speed:");
-  Serial.print(speed);
+  debug("move()- dir:");
+  debug(dir);
+  debug(" dist_ticks:");
+  debug(dist_ticks);
+  debug(" blocked_dist_cm:");
+  debug(blocked_dist_cm);
+  debug(" speed:");
+  debug(speed, 1);
   
   //slowSpeed calculates how fast the slower motor will spin while the bot is turning.
   int slowSpeed = (speed/2)+(speed/4)+(speed/8);
@@ -249,12 +250,12 @@ void move(int dir, int dist_ticks, int blocked_dist_cm, int speed){
     avg_ticks_from_start = ((encoder.getTicks(LEFT)-encoderStart[0]) 
                                 + (encoder.getTicks(RIGHT)-encoderStart[1]) / 2);
                      
-    Serial.print("currentHeading:");
-    Serial.print(compass.getHeading());           
-    Serial.print(" Avg Ticks:");
-    Serial.print(avg_ticks_from_start);
-    Serial.print(" f-ping:");
-    Serial.println(distanceSensor.f);
+    debug("currentHeading:");
+    debug(compass.getHeading());           
+    debug(" Avg Ticks:");
+    debug(avg_ticks_from_start);
+    debug(" f-ping:");
+    debug(distanceSensor.f, 1);
     
     // If ping is too small, and not 0, or if our avg ticks are to big we will stop moving
     if(((distanceSensor.f < blocked_dist_cm && distanceSensor.f != 0) 
@@ -268,12 +269,12 @@ void move(int dir, int dist_ticks, int blocked_dist_cm, int speed){
     
   }
   motors.stop();
-  Serial.print("DONE: move()- currentHeading:");
-  Serial.print(compass.getHeading());
-  Serial.print(" f-ping:");
-  Serial.print(distanceSensor.f);
-  Serial.print(" avg_ticks_from_start:");
-  Serial.println(avg_ticks_from_start);
+  debug("DONE: move()- currentHeading:");
+  debug(compass.getHeading());
+  debug(" f-ping:");
+  debug(distanceSensor.f);
+  debug(" avg_ticks_from_start:");
+  debug(avg_ticks_from_start, 1);
 }
 
 // calculates the correct turn to make to reach the goal from the current rotation
@@ -291,7 +292,7 @@ int getTurn(float cur, float goal){
 
 void turnTo(float dir, int n){
   float heading;
-  Serial.println(" turnTo: ");
+  debug(" turnTo: ", 1);
   if(n > 3) return;
   heading = compass.getHeading();
   int t = getTurn(heading, dir);
@@ -308,16 +309,16 @@ void turnTo(float dir, int n){
     }else{
       mag = map(abs(t), 0, 180, 140, 170);
     }
-  Serial.print(" N: ");
-  Serial.print(n);
-  Serial.print(" T: ");
-  Serial.print(t);
-  Serial.print(" MAG: ");
-  Serial.print(mag);
-  Serial.print(" CurrentHeading ");
-  Serial.print(heading);
-  Serial.print(" GoalHeading: ");
-  Serial.println(dir);
+  debug(" N: ");
+  debug(n);
+  debug(" T: ");
+  debug(t);
+  debug(" MAG: ");
+  debug(mag);
+  debug(" CurrentHeading ");
+  debug(heading);
+  debug(" GoalHeading: ");
+  debug(dir, 1);
     if( t >= 2){
        motors.pivot(-mag); 
     }else{
@@ -332,14 +333,22 @@ void turnTo(float dir, int n){
   heading = compass.getHeading();
   t = getTurn(heading, dir);
   if(abs(t) > 5) turnTo(dir, n+1);
-  Serial.print("TurnToDone h:");
-  Serial.print(heading);
-  Serial.print(" Goal: ");
-  Serial.print(dir);
-  Serial.print(" N: ");
-  Serial.println(n);
+  debug("TurnToDone h:");
+  debug(heading);
+  debug(" Goal: ");
+  debug(dir);
+  debug(" N: ");
+  debug(n);
  
 }
 
-
-
+/** Used to display debug values to Serial. */
+void debug(String mesg, bool newLine = false){
+  #ifdef DEBUG
+    if(newLine){
+       Serial.println(mesg);
+    }else{
+       Serial.print(mesg); 
+    }
+  #endif
+}
